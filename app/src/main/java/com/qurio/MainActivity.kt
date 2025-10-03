9220b846918c34a9127fa5219816d921df26f55b
MuhammedWael9991 <mw26200400@gmail.com>
2025-10-03 22:28:30 +0300

    Add onboarding fragment and navigation setup in MainActivity

--- app/src/main/java/com/qurio/MainActivity.kt
+++ app/src/main/java/com/qurio/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.qurio
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.qurio.ui.screen.onbording.OnboardingFragment
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val prefs = getSharedPreferences("qurio_prefs", MODE_PRIVATE)
+        val isFirstLaunch = prefs.getBoolean("first_launch", true)
+        Log.d("prefs", "isFirstLaunch: $isFirstLaunch")
+
+        if (isFirstLaunch) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.onboardingFragment, OnboardingFragment())
+                .commit()
+
+            prefs.edit { putBoolean("first_launch", false) }
+        } else {
+           // Load the main content fragment or activity
         }
     }
 }
