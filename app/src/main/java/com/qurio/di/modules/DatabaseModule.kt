d4b0fcc1553e68927b82c6b963d45b0f320a59bd
MuhammedWael9991 <mw26200400@gmail.com>
2025-10-12 18:45:31 +0300

    Add DAOs for Characters and Achievements to DatabaseModule

--- app/src/main/java/com/qurio/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/qurio/di/modules/DatabaseModule.kt
@@ -2,6 +2,8 @@ package com.qurio.di.modules
 
 import android.app.Application
 import androidx.room.Room
+import com.qurio.data.local.dao.AchievementsDao
+import com.qurio.data.local.dao.CharactersDao
 import com.qurio.data.local.dao.UserDao
 import com.qurio.data.local.database.AppDatabase
 import dagger.Module
@@ -20,4 +22,14 @@ object DatabaseModule {
 
     @Provides
     fun provideUserDao(db: AppDatabase): UserDao = db.userDao()
+
+    @Provides
+    fun provideCharactersDao(db: AppDatabase): CharactersDao {
+        return db.charactersDao()
+    }
+
+    @Provides
+    fun provideAchievementsDao(db: AppDatabase): AchievementsDao {
+        return db.achievementsDao()
+    }
 }
